bc9a48da3495eea72e83a15a9f5a973ae6e35bee
dbdodnb <daniilmamentovich@gmail.com>
2022-11-11 21:07:58 +0200

    .

---
+++ js/script.js
@@ -0,0 +1,12 @@
+VANTA.TRUNK({
+    el: "#header",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xa29d9e,
+    spacing: 1.00
+  })
